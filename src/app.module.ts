5f9e2ff5a5fc7d3d6533870afc36841703116437
Frédéric Jacques <gary.bob88@gmail.com>
2022-08-26 22:56:28 -0400

    add chaos engineeringMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { ChoasEngineesingMiddleware } from './middlewares/choasEngineesingMiddleware';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot({
+      load: [configuration]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ChoasEngineesingMiddleware)
+      .forRoutes('*');
+  }
+}
---
+++ src/middlewares/choasEngineesingMiddleware.ts
@@ -0,0 +1,20 @@
+
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class ChoasEngineesingMiddleware implements NestMiddleware {
+  constructor(private configService: ConfigService) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const percentageError = this.configService.get<number>('chaos.percentageError')
+    const randomNumber = Math.random()
+    if (randomNumber > percentageError) {
+      next()
+    }
+    else {
+      res.status(Math.floor(Math.random() * (500 - 400 + 1)) + 400).send()
+    }
+  }
+}
