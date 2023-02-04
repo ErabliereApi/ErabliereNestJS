f6d205bcdd3ec8ab99360cf53d6575a0bab152f1
Frédéric Jacques <gary.bob88@gmail.com>
2023-02-04 08:51:24 -0500

    improve logger memory usage

--- src/app/logger/app.logger.ts
+++ src/app/logger/app.logger.ts
@@ -1,4 +1,4 @@
-import { ConsoleLogger, Injectable, Logger, LoggerService, Scope } from "@nestjs/common";
+import { Injectable, LoggerService, Scope } from "@nestjs/common";
 import { ContextLogger } from "./context.logger";
 import { ConfigService } from "@nestjs/config";
 import { getWinstonLogger } from "./config.functions";
@@ -6,47 +6,49 @@ import { getWinstonLogger } from "./config.functions";
 @Injectable({ scope: Scope.REQUEST })
 export class AppLogger implements LoggerService {
 
-  private readonly logger: LoggerService
+  private static logger: LoggerService
 
   /**
    *
    */
   constructor(private readonly context: ContextLogger, private readonly config: ConfigService) {
-    this.logger = getWinstonLogger(config)
+    if (!AppLogger.logger) {
+      AppLogger.logger = getWinstonLogger(config)
+    }
   }
 
   /**
    * Write a 'log' level log.
    */
   log(message: any, ...optionalParams: any[]) {
-    this.logger.log(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams]);
+    AppLogger.logger.log(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams]);
   }
 
   /**
    * Write an 'error' level log.
    */
   error(message: any, ...optionalParams: any[]) {
-    this.logger.error(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
+    AppLogger.logger.error(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
   }
 
   /**
    * Write a 'warn' level log.
    */
   warn(message: any, ...optionalParams: any[]) {
-    this.logger.warn(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
+    AppLogger.logger.warn(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
   }
 
   /**
    * Write a 'debug' level log.
    */
   debug?(message: any, ...optionalParams: any[]) {
-    this.logger.debug(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
+    AppLogger.logger.debug(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
   }
 
   /**
    * Write a 'verbose' level log.
    */
   verbose?(message: any, ...optionalParams: any[]) {
-    this.logger.verbose(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
+    AppLogger.logger.verbose(message, [{trackingId: this.context.trackingId}, {transactionId: this.context.transationId}, ...optionalParams])
   }
 }
